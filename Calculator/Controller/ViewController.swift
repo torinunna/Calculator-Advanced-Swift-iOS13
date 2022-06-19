5c6eb555bc988b7110ca1327cf7f2fa78bd09688
torinunna <torinunna9588@gmail.com>
2022-06-19 23:44:54 +0900

    Conform to MVC - struct CalculatorLogic - edit calcMethod

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -35,14 +35,14 @@ class ViewController: UIViewController {
         isFinishedTypingNumber = true
         
         if let calcMethod = sender.currentTitle {
-            if calcMethod == "+/-" {
-                displayValue *= -1
-            } else if calcMethod == "AC" {
-                displayLabel.text = "0"
-            } else if calcMethod == "%" {
-                displayValue *= 0.01
+            
+            let calculator = CalculatorLogic(number: displayValue)
+            
+            guard let result = calculator.calculate(symbol: calcMethod) else {
+                fatalError("The result of the calculation is nil")
             }
             
+            displayValue = result
         }
         
     }
--- Calculator/Model/CaculatorLogic.swift
+++ Calculator/Model/CaculatorLogic.swift
@@ -8,8 +8,26 @@
 
 import Foundation
 
-class CalculatorLogic {
+struct CalculatorLogic {
     
+    var number: Double
     
+    init(number: Double) {
+        self.number = number
+    }
+    
+    func calculate(symbol: String) ->  Double? {
+        
+            if symbol == "+/-" {
+                return number * -1
+            } else if symbol == "AC" {
+                return 0
+            } else if symbol == "%" {
+                return number * 0.01
+            }
+         
+        return nil
+        
+        }
     
 }
