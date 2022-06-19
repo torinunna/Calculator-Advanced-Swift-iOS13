36aa220262feb0d2dc20cab04b55f58408242288
torinunna <torinunna9588@gmail.com>
2022-06-19 17:04:39 +0900

    conform to MVC

---
+++ Calculator/Model/CaculatorLogic.swift
@@ -0,0 +1,15 @@
+//
+//  CaculatorLogic.swift
+//  Calculator
+//
+//  Created by 권유진 on 2022/06/19.
+//  Copyright © 2022 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorLogic {
+    
+    
+    
+}
