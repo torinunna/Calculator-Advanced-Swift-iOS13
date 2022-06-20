135c10bb023ec9fb43036e8e36109202d5d6c0c5
torinunna <torinunna9588@gmail.com>
2022-06-20 16:59:09 +0900

    - Guard let vs If let - change if statement to switch

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -38,11 +38,9 @@ class ViewController: UIViewController {
         
         if let calcMethod = sender.currentTitle {
             
-            guard let result = calculator.calculate(symbol: calcMethod) else {
-                fatalError("The result of the calculation is nil")
+            if let result = calculator.calculate(symbol: calcMethod) {
+                displayValue = result
             }
-            
-            displayValue = result
         }
         
     }
