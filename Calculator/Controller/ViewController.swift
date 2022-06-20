0d01bf6941ea7fd75f4982c1f3c167ea8dc0083e
torinunna <torinunna9588@gmail.com>
2022-06-20 16:06:47 +0900

    turn CalculatorLogic into a struct

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
         }
     }
     
-    
+    private var calculator = CalculatorLogic()
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
@@ -34,9 +34,9 @@ class ViewController: UIViewController {
         
         isFinishedTypingNumber = true
         
+        calculator.setNumber(displayValue)
+        
         if let calcMethod = sender.currentTitle {
-            
-            let calculator = CalculatorLogic(number: displayValue)
             
             guard let result = calculator.calculate(symbol: calcMethod) else {
                 fatalError("The result of the calculation is nil")
--- Calculator/Model/CaculatorLogic.swift
+++ Calculator/Model/CaculatorLogic.swift
@@ -10,22 +10,23 @@ import Foundation
 
 struct CalculatorLogic {
     
-    var number: Double
+    private var number: Double?
     
-    init(number: Double) {
+    mutating func setNumber(_ number: Double) {
         self.number = number
     }
     
     func calculate(symbol: String) ->  Double? {
         
+        if let n = number {
             if symbol == "+/-" {
-                return number * -1
+                return n * -1
             } else if symbol == "AC" {
                 return 0
             } else if symbol == "%" {
-                return number * 0.01
+                return n * 0.01
             }
-         
+        }
         return nil
         
         }
